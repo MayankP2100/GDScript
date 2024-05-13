58c39afc0b7a37763a2ecbc5b88b7d3ca4369260
Mayank <palmayank2100@gmail.com>
2024-05-13 11:33:32 +0530

    2. Modifying nodes 1.0
    Added a label to display "Hello, world!" and changed the text and color through script, and changed font size through label inspector.

--- main.gd
+++ main.gd
@@ -3,5 +3,5 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("Hello, world!")
-
+	$Label.text = "Hello, world!"
+	$Label.modulate = Color.GREEN
