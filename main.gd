4dc7242117a0bad07d781c66131ebc584264bdaa
Mayank <palmayank2100@gmail.com>
2024-05-13 12:21:29 +0530

    5. if-statements
    Implemented if-statements to color code the health label and stop health to drop below 0.

--- main.gd
+++ main.gd
@@ -9,5 +9,18 @@ func _ready() -> void:
 	
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed('my_action'):
-		health -= 20
+		health -= 15
+
+		if health < 0:
+			health = 0
+
+		if health > 70:
+			$Label.modulate = Color.GREEN
+		elif health <= 70 and health > 30:
+			$Label.modulate = Color.YELLOW
+		elif health <= 30 and health > 0:
+			$Label.modulate = Color.RED
+		elif health <= 0:
+			$Label.modulate = Color.BLACK
+
 		$Label.text = str(health)
